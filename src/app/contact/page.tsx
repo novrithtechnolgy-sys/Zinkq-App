e722cdd1120a7a66a95f26dcb154a31cad4cdd42
GSNSAMPATH <sumalnadira123@gmail.com>
2025-11-20 18:35:36 +0530

    update email

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,17 @@
+export default function Contact() {
+    return (
+        <section className="w-full md:py-20 flex justify-center px-4">
+            <div className="max-w-3xl text-center">
+                <h2 className="font-roboto text-[20px] md:text-[38px] lg:text-[56px] font-bold text-[#1B1B1D] mb-6">
+                    Contact
+                </h2>
+                <p className="font-roboto  text-gray-600 leading-relaxed text-[14px] md:text-[16px] lg:text-[20px] pb-8">
+                    Zinkq is more than a platform. it’s a movement. We connect Sri Lanka’s most
+                    ambitious founders, startups, and freelancers to collaborate, share knowledge, 
+                    and grow together. It’s your space to meet like-minded innovators, gain 
+                    visibility, and access the support every entrepreneur deserves.
+                </p>
+            </div>
+        </section>
+    );
+}

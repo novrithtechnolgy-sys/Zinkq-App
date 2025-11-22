84fc52071f6d08e406af093ec985ff4703c474ed
GSNSAMPATH <sumalnadira123@gmail.com>
2025-11-22 21:56:18 +0530

    build next step 2

--- src/app/components/About/community.tsx
+++ src/app/components/About/community.tsx
@@ -3,7 +3,7 @@ import { FaC, FaCircleCheck } from "react-icons/fa6";
 export default function Community() {
     return (
         <section className="mx-2 md:mx-4 lg:mx-8 rounded-b-3xl py-4 md:py-10">
-            <div className="md:px-10 xl:px-12 2xl:px-20 mx-auto px-6 rounded-xl md:rounded-3xl bg-[#F0F2F4] w-full py-10 md:py-10 ">
+            <div className="md:px-10 xl:px-12 2xl:px-20 mx-auto px-4 rounded-xl md:rounded-3xl bg-[#F0F2F4] w-full py-10 md:py-10 ">
                 <div className="max-w-4xl">
         <h2 className="font-roboto text-[20px] md:text-[38px] lg:text-[56px] font-bold text-[#1A1A1A]">
           Our Community

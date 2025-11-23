0f2d9526356fa8ace13f7ae6813c30acbfe7ae57
GSNSAMPATH <sumalnadira123@gmail.com>
2025-11-23 22:52:03 +0530

    update icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export const metadata: Metadata = {
       {url: "/favicon.ico", type: "image/x-icon"},
       {url: "/favicon-16x16.png", sizes: "16x16", type: "image/png"},
       {url: "/favicon-48.png", sizes: "48x48", type: "image/png"},
-      {url: "/favicon-192.png", sizes: "192x192", type: "image/png"}
+      {url: "/favicon-192.png", sizes: "192x192", type: "image/png"},
       {url: "/favicon-512.png", sizes: "512x512", type: "image/png"}
     ],
     shortcut: "/favicon-16x16.png",
